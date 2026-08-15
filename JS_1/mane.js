85a6baa822063bb71bf518f560087bd983c8a6be
lukaninmatvej40-png <lukaninmatvej40@gmail.com>
2026-08-15 23:42:58 +0300

    Add files via upload

---
+++ JS_1/mane.js
@@ -0,0 +1,30 @@
+// №1
+const firstName = "Леонеля";
+const lastName = "Пепси";
+const isStudent = true;
+console.log(firstName);
+console.log(lastName);
+console.log(isStudent);
+
+// №2
+let age = 67;
+let currentYear = 2026;
+let birthYear = currentYear - age;
+console.log(age);
+console.log(currentYear);
+console.log(birthYear);
+
+// №3
+console.log(
+  "Меня зовут " +
+    firstName +
+    " " +
+    lastName +
+    ", мне " +
+    age +
+    " лет. Я ученик курса: " +
+    isStudent,
+);
+
+// №4
+console.log(123456789 + "falsetrue");
